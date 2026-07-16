9fa33f23cfcd27383ed28228d5a09b9441652af3
bighollow <patersonj3132@masseyhigh.school.nz>
2026-07-17 09:33:20 +1200

    finished code (I think)

--- B_01_price_comparer.py
+++ B_01_price_comparer.py
@@ -1,3 +1,5 @@
+import pandas
+
 def not_blank(question):
     """checks that user response is not blank"""
     while True:
@@ -49,6 +51,7 @@ def instructions():
     
     please enter your budget, the cost of each item, and the weight of each item,
     then you will be given the best item you could buy with your budget. 
+    please use all the same unit. 
     ''')
 
 def yes_no(question):
@@ -63,9 +66,9 @@ def yes_no(question):
         else:
             print("please enter yes (y) or no (n) \n")
 
-def string_check(question, valid_ans_list):
+def string_check(question, valid_ans_list, num_letters):
     """checks that the user enters the full word
-    or first letter of a word fom a list of responses"""
+    or 'n' letter/s of a word fom a list of responses"""
 
     while True:
 
@@ -75,10 +78,28 @@ def string_check(question, valid_ans_list):
             if response == item:
                 return item
 
-            elif response == item[0]:
+            elif response == item[:num_letters]:
                 return item
 
         print(f"please choose an option from {valid_ans_list}")
+
+# main routine
+
+#lists for pandas
+all_items = []
+all_amounts = []
+all_units = []
+all_prices = []
+all_prices_per = []
+
+price_clac_dict = {
+    "item": all_items,
+    "amount": all_amounts,
+    "unit": all_units,
+    "price": all_prices,
+    "priceper": all_prices_per
+}
+
 
 # Heading (don't use statement generator)
 print("💵💵💵price compare tool💵💵💵")
@@ -94,9 +115,9 @@ budget = num_checker("please enter your budget:")
 
 print(f"your budget is {budget}")
 
-# Loop!
+# Loop for inputs
 while True:
-    #ask user for their name (and check it's not blank)
+    #ask user for the name of the item (and check it's not blank)
     print()
     item = not_blank("name of item: ")
 
@@ -105,9 +126,36 @@ while True:
         break
     #get unit (blank is each)
     unit = string_check("please enter your unit:",
-                        ['grams','kilograms', 'mills','liters',''  ])
-
+                        ['grams','kilograms', 'mills','liters',''  ], 1)
+    # get amount of product
     amount = num_checker("please enter the weight/volume/amount of the item:")
+    # get price of product
+    cost_input = num_checker("please enter the cost of the item:")
+    # convert cost to correct formating
+    cost = f"${cost_input:.2f}"
+    # get price per unit and convert to currency format.
+    cost_per_raw = cost_input / amount
+    cost_per = f"${cost_per_raw:.2f}"
 
-    cost = num_checker("please enter the cost of the item:".sum)
+    all_items.append(item)
+    all_amounts.append(amount)
+    all_units.append(unit)
+    all_prices.append(cost)
+    all_prices_per.append(cost_per)
+
+#create table
+price_compare_frame = pandas.DataFrame(price_clac_dict)
+#remove unneeded index
+price_compare_table = price_compare_frame.to_string(index=False)
+
+best = min(all_prices_per)
+
+print(price_compare_table)
+
+print(f"the best item you can buy costs {best} per unit")
+
+max_buyable = budget / best
+print(max_buyable)
+
+
 
